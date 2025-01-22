c2828af0db830be6d26ac702a923bf274e6821ce
Roshan R Chandar <roshanr2001@gmail.com>
2025-01-22 14:44:55 +0530

    docs(build): mock autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
+autodoc_mock_imports = ["bson", "pymongo"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
